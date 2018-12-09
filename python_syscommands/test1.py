d69edd7d5bccb551d347e9f924e4ced7de31bc93
Patrick Hanlan <pat@patrickhanlan.com>
2018-12-09 13:24:26 +0000

    Bluetoothctl now connects to the speaker. I want to get this working without having to close the stdin buffer. Also need to clean up the code.

--- python_syscommands/test1.py
+++ python_syscommands/test1.py
@@ -3,13 +3,33 @@
 import subprocess, time
 
 speakerGUID = '04:52:C7:21:CA:4A'
-testSubprocess = subprocess.Popen(['bluetoothctl'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+testSubprocess = subprocess.Popen(['bluetoothctl'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 
 # Reference - https://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python
-while True:  # lets wait for 'user' prompt
-    line = testSubprocess.stdout.readline().decode("unicode_escape")
-    print(line)
-    if line.find('Agent registered') >= 0:
+line = ''
+lastChar = ''
+isReady = False
+sent = False
+while sent == False:  # lets wait for 'user' prompt
+    lastChar = testSubprocess.stdout.read(1).decode("unicode_escape")
+    line = line + lastChar
+    if isReady == True:
+        if lastChar == '#':
+            print(line)
+            commandString = "connect {}\n".format(speakerGUID)
+            print(commandString)
+            testSubprocess.stdin.write(commandString.encode())
+            testSubprocess.stdin.close()
+            isReady = False
+            print(testSubprocess.poll())
+            sent = True
+    elif lastChar == '\n':
+        line = line.rstrip('\n')
+        if line.find('Agent registered') >= 0:
+            isReady = True
+        print(line)
+        line = ''
+    #if line.find('Agent registered') >= 0:
         # https://stackoverflow.com/questions/510348/how-can-i-make-a-time-delay-in-python
-        time.sleep(2)
-        testSubprocess.stdin.write('connect {}\n'.format(speakerGUID).encode())
+    #    time.sleep(2)
+    #    testSubprocess.stdin.write('connect {}\n'.format(speakerGUID).encode())
