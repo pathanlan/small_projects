1f2c43dddab0d9eb181b28bdd1170c7e45fbca94
Patrick Hanlan <pat@patrickhanlan.com>
2018-12-09 12:11:11 +0000

    Trashing about trying to get bluetoothctl to work, saw an example using rfcomm which may work better, switching to that...

--- python_syscommands/test1.py
+++ python_syscommands/test1.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # Reference - https://www.python-course.eu/os_module_shell.php
+import subprocess, time
 
-import subprocess
+speakerGUID = '04:52:C7:21:CA:4A'
+testSubprocess = subprocess.Popen(['bluetoothctl'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
 
-# Reference - https://stackoverflow.com/questions/3172470/actual-meaning-of-shell-true-in-subprocess
-testSubprocess = subprocess.check_output(['bluetoothctl'],input=b'exit')
+# Reference - https://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python
+while True:  # lets wait for 'user' prompt
+    line = testSubprocess.stdout.readline().decode("unicode_escape")
+    print(line)
+    if line.find('Agent registered') >= 0:
+        # https://stackoverflow.com/questions/510348/how-can-i-make-a-time-delay-in-python
+        time.sleep(2)
+        testSubprocess.stdin.write('connect {}\n'.format(speakerGUID).encode())
