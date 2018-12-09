f0f928091fb7868174966bd15ab01fb6db9eba67
Patrick Hanlan <pat@patrickhanlan.com>
2018-12-09 12:14:16 +0000

    RFCOMM needs sudo, so bluetoothctl is preferable. May switch back to that

--- python_syscommands/test1.py
+++ python_syscommands/test1.py
@@ -3,13 +3,4 @@
 import subprocess, time
 
 speakerGUID = '04:52:C7:21:CA:4A'
-testSubprocess = subprocess.Popen(['bluetoothctl'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
-
-# Reference - https://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python
-while True:  # lets wait for 'user' prompt
-    line = testSubprocess.stdout.readline().decode("unicode_escape")
-    print(line)
-    if line.find('Agent registered') >= 0:
-        # https://stackoverflow.com/questions/510348/how-can-i-make-a-time-delay-in-python
-        time.sleep(2)
-        testSubprocess.stdin.write('connect {}\n'.format(speakerGUID).encode())
+testSubprocess = subprocess.run(['rfcomm','connect','0',speakerGUID,'1'])
