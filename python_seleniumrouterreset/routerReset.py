8199a860e643d51e0e615cac82ade2a8d974a4ce
Patrick Hanlan <pat@patrickhanlan.com>
2018-12-12 09:17:35 +0000

    * Added Selenium Router Reset folder, basic outline of a script and psuedo code * Added VS Code GitIgnore - not sure this is doing everything I want yet

---
+++ python_seleniumrouterreset/routerReset.py
@@ -0,0 +1,13 @@
+from credentials import myRouterCredentials
+
+# Log in to the router admin site
+
+# Check if the internet is connected
+
+# If the internet is not connected, go to the internet status page
+
+# Disconnect the internet
+
+# Connect the internet
+
+# Verify the internet is connected
